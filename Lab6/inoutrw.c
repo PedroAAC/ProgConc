4e7cf0843f72edf90aefc32c2ac128abf7a74d70
PedroAAC <54105523+PedroAAC@users.noreply.github.com>
2024-10-03 09:51:53 -0300

    Create inoutrw.c

---
+++ Lab6/inoutrw.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void EntraLeitor(){
+}
+
+void SaiLeitor(){
+}
+
+int EntraEscritor(int escritores){
+    if(escritores==0){
+        escritores==1;
+    }
+}
+
+int SaiEscritor(int escritores){
+}
